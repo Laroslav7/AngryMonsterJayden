629587bb9e4c8e7fb763bda53d1f334e87e9bc94
Laroslav7 <microsoftjfdjg7@gmail.com>
2026-08-09 02:17:46 +0300

    Initial commit

---
+++ app/src/main/java/com/laroslav/angrymonsterjayden/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.laroslav.angrymonsterjayden
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.laroslav.angrymonsterjayden.ui.theme.AngryMonsterJaydenTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            AngryMonsterJaydenTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    AngryMonsterJaydenTheme {
+        Greeting("Android")
+    }
+}
